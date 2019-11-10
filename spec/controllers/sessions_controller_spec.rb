525f760097478fd835e575f43ed235ec2b8efb1d
Patrick Baselier <patrick.baselier@gmail.com>
2019-11-10 11:13:05 +0100

    Test sessions controller

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -3,5 +3,21 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
-  pending "add some examples to (or delete) #{__FILE__}"
+  render_views
+
+  describe 'GET #new' do
+    def do_get
+      get :new
+    end
+
+    it 'returns a success response' do
+      do_get
+      expect(response).to be_successful
+    end
+
+    it 'assigns a `user` variable' do
+      do_get
+      expect(assigns[:user]).to be_a(User)
+    end
+  end
 end
