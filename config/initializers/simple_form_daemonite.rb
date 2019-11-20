69d280800ae28ce4f5aba36a6819de905657bb7c
Patrick Baselier <patrick.baselier@gmail.com>
2019-11-20 12:26:34 +0100

    Experiment w/ text field boxes

--- config/initializers/simple_form_daemonite.rb
+++ config/initializers/simple_form_daemonite.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 SimpleForm.setup do |config|
-  # Custom wrapper to support floating label text fields
-  # See http://daemonite.github.io/material/docs/4.1/material/text-fields/#floating-label-text-fields.
-  config.wrappers :vertical_form_w_floating_label, tag: 'div', class: 'form-group floating-label', error_class: 'form-group-invalid', valid_class: 'form-group-valid' do |b|
+  # Custom wrapper to support text field boxes
+  # See http://daemonite.github.io/material/docs/4.1/material/text-fields/#text-field-boxes
+  config.wrappers :vertical_form_w_text_field_boxes, tag: 'div', class: 'form-group', error_class: 'form-group-invalid', valid_class: 'form-group-valid' do |b|
     b.use :html5
     b.use :placeholder
     b.optional :maxlength
@@ -11,10 +11,12 @@ SimpleForm.setup do |config|
     b.optional :pattern
     b.optional :min_max
     b.optional :readonly
-    b.use :label
-    b.use :input, class: 'form-control', error_class: 'is-invalid', valid_class: 'is-valid'
-    b.use :full_error, wrap_with: { tag: 'div', class: 'invalid-feedback' }
-    b.use :hint, wrap_with: { tag: 'small', class: 'form-text text-muted' }
+    b.wrapper tag: :div, class: 'textfield-box' do |c|
+      c.optional :label
+      c.use :input, class: 'form-control', error_class: 'is-invalid', valid_class: false
+      c.use :full_error, wrap_with: { tag: 'div', class: 'invalid-feedback' }
+      c.use :hint, wrap_with: { tag: 'small', class: 'form-text text-muted' }
+    end
   end
 
   config.wrappers :toggle_buttons, tag: :div, class: 'btn-group form-group', item_wrapper_tag: false, html: { data: { toggle: 'buttons'}, role: 'group' } do |b|
@@ -24,6 +26,6 @@ SimpleForm.setup do |config|
   end
 
   # The default wrapper to be used by the FormBuilder.
-  config.default_wrapper = :vertical_form_w_floating_label
+  config.default_wrapper = :vertical_form_w_text_field_boxes
 end
 
