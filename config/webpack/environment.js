12f3d1531256717144a1f7588e631f13e33603b4
Patrick Baselier <patrick.baselier@gmail.com>
2020-01-05 10:45:09 +0100

    Add font-awesome

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -15,4 +15,12 @@ environment.loaders.append('expose', {
   ],
 });
 
+// resolve-url-loader must be used before sass-loader
+environment.loaders.get('sass').use.splice(-1, 0, {
+  loader: 'resolve-url-loader',
+  options: {
+    attempts: 1,
+  },
+});
+
 module.exports = environment;
